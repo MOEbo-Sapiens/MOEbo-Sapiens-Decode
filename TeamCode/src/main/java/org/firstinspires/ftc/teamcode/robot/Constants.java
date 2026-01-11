c1fe7d8cf0ce686076b03353b07e03353e3a6dd6
Kabir Goyal <kabirgoyal@icloud.com>
2026-01-11 01:16:25 -0500

    constants updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
@@ -7,24 +7,31 @@ import com.pedropathing.geometry.Pose;
 public class Constants {
     static {
         // SHOOTER CONSTANTS
-        double MIN_SHOOTER_ARC = Math.toRadians(40);
-        double MAX_SHOOTER_ARC = Math.toRadians(80);
+        double STRAIGHT_TRAVEL_DISTANCE = 2; // INCHES, TODO: Calculate this
+        double STRAIGHT_TRAVEL_TIME = 0.5; // SECONDS TODO: Calculate this
 
-        double MAX_SHOOTER_ANGLE = Math.toRadians(50);
-        double MIN_SHOOTER_ANGLE = Math.toRadians(10);
+        double MIN_SHOOTER_ANGLE = Math.toRadians(40);
+        double MAX_SHOOTER_ANGLE = Math.toRadians(80);
+        double FLYWHEEL_FIRST_CONTACT_ANGLE = Math.toRadians(-18.241);
 
+        double MAX_LAUNCH_ANGLE = Math.toRadians(50);
+        double MIN_LAUNCH_ANGLE = Math.toRadians(10);
+
+        // MIN_SHOOTER_ARC -> MAX_SHOOTER_ANGLE
+        // MAX_SHOOTER_ARC -> MIN_SHOOTER_ANGLE
         // 40 arc -> 50 angle
         // 80 arc -> 10 angle
 
-        double CONTACT_ARC_ANGLE = Math.toRadians(-18.241);
         double FLYWHEEL_RADIUS = 1.41732; // INCHES
         double BALL_RADIUS = 2.5; // INCHES
-        double GRAVITY = 386.0885; // INCHES PER SECOND SQUARED
         double TOTAL_RADIUS = FLYWHEEL_RADIUS + BALL_RADIUS;
 
-        Pose BLUE_GOAL_POSE = new Pose(10, 134, Math.toRadians(180));
-        Pose RED_GOAL_POSE = new Pose(134, 134, Math.toRadians(180));
-        double GOAL_HEIGHT = 47.5; // INCHES
+        Pose BLUE_GOAL_POSE = new Pose(10, 134, Math.toRadians(0));
+        Pose RED_GOAL_POSE = new Pose(134, 134, Math.toRadians(0));
+
+
+        double GOAL_HEIGHT = 38.75; // INCHES
+        double GRAVITY = 386.0885; // INCHES PER SECOND SQUARED
     }
 
     public static Robot robot;
